5f19b9bbce9e539c089b5c0887e936c690c98063
Irum F <irummf@gmail.com>
2018-09-01 21:55:19 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
-require 'pry'
 
 class String
-
+ 
+  def initialize(sentence)
+     @sentence = sentence 
+  end 
+   
   def sentence?
-    
+    self.end_with?(".")
   end
-
+ 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
-end
+    self.split(/\.|\!|\?/).reject{|string| string.empty?}.length  
+  end 
+end 
